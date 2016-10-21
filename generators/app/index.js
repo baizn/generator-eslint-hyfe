3c22b2156e97822ae8aa94008593678169ed02e0
baizn <cq_bzhn0214@sina.com>
2016-10-21 15:31:28 +0800

    update files

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,11 +1,13 @@
+
+require('colors')
 var generator = require('yeoman-generator')
 var path = require('path')
 var fs = require('fs')
-var colors = require('colors')
 var prompts = require('./prompts')
 var plato = require('plato-zh')
 var hyfeRules = require('eslint-config-hyfe')
 
+//var SPLIT_COUNT = 25
 /**
  * 执行顺序：
  *  1 initializing - Your initialization methods
@@ -18,98 +20,147 @@ var hyfeRules = require('eslint-config-hyfe')
  *  8 end - Called last, cleanup, say good bye
  */
 module.exports = generator.Base.extend({
-    constructor: function() {
-        generator.Base.apply(this, arguments)
-        this.params = {}
-        this.stdouts = {}
-    },
+  constructor: function() {
+    generator.Base.apply(this, arguments)
+    this.params = {}
+    this.stdouts = {}
+  },
 
-    prompting: {
-        questions: function () {
-            var next = this.async()
-            return this.prompt(prompts).then(function (answers) {
-                
-                if(!answers.action) {
-                    console.log('\033[31m 终止扫描' + answers.inputPath + '项目\033[39m\n')
-                    return false
-                }
-                this.log(' ')
-                this.log(('正在扫描检测' + answers.inputPath + '项目中的JavaScript文件，请稍后...').yellow.bold)
-                this.log(' ')
-                this.params = answers
+  prompting: {
+    questions: function () {
+      var next = this.async()
+      this.log(' ')
+      this.log(('友情提示：所选项目文件内容不要过大, 以防内存溢出(nodejs在64位系统下默认只有1.4G左右内存)！').red.bold)
+      this.log(' ')
+      return this.prompt(prompts).then(function (answers) {      
+        if(!answers.action) {
+          this.log('\033[31m 终止扫描' + answers.inputPath + '项目\033[39m\n')
+          return false
+        }
+        this.log(' ')
+        this.log(('正在扫描检测' + answers.inputPath + '项目中的JavaScript文件，请稍后...').yellow.bold)
+        this.log(' ')
+        this.params = answers
                
-                next()
-            }.bind(this)) 
+        next()
+      }.bind(this)) 
+    }
+  },
+
+  checkingFiles: function() {
+    var self = this
+
+    var filePath = this.params.inputPath || process.cwd()
+
+    var fileterFolders = this.params.filterPath
+    var filterFile = this.params.file
+    if(filterFile) {
+      fileterFolders.push('(^\\.[_a-zA-Z0-9+])')
+    }
+    var reg = new RegExp(fileterFolders.join('|'), 'i')
+
+    var allJsFiles = this._getAllJsFiles(filePath, reg)
+    
+    //使用自定义规则扫描所有JS文件，并生成报告
+    var outputDir = this.params.outputPath + '/reports' || process.cwd() + '/reports'
+
+    this.stdouts.inputPath = filePath
+    this.stdouts.outputPath = outputDir
+
+    var useRules = hyfeRules
+
+    //使用ES6/7语法并执行JSX
+    if(this.params.es6more && this.params.isJsx) {
+      useRules.parserOptions = {
+        ecmaVersion: 7,
+        sourceType: 'module',
+        ecmaFeatures: {
+          experimentalObjectRestSpread: true,
+          jsx: true
         }
-    },
+      }
+    } else if(this.params.isJsx) {
+      useRules.parserOptions = {
+        ecmaFeatures: {
+          experimentalObjectRestSpread: true,
+          jsx: true
+        }
+      }
+    }
 
-    writing: {
-        checkingFiles: function() {
-            var self = this
-            //读取输入目录下的所有js文件
-            var filePath = this.params.inputPath || process.cwd()
+    if(this.params.isReact) {
+      useRules.plugins = ['react']
+      useRules.extends = ['eslint:recommended', 'plugin:react/recommended']
+    }
 
-            var allJsFiles = this._getAllJsFiles(filePath)
+    var options = {
+      title: this.params.reportTitle,
+      eslint: useRules
+    }
 
-            //使用自定义规则扫描所有JS文件，并生成报告
-            var outputDir = this.params.outputPath + '/output' || process.cwd() + '/output'
+    // var jsFileNums = allJsFiles.length
+    // var num = 0
+    // var reports = []
+    // var len = Math.ceil(allJsFiles.length / SPLIT_COUNT)
 
-            this.stdouts.inputPath = filePath
-            this.stdouts.outputPath = outputDir
+    var callback = function() {
+      // var util = require('util')
 
-            var options = {
-                title: this.params.reportTitle,
-                eslint: hyfeRules
-            }
-
-            var callback = function(report) {
-                console.log((self.params.reportTitle + 'JavaScript代码扫描完成......').green.bold)
-                console.log(' ')
-                console.log(('===================' + self.params.reportTitle + '基本信息===================').green.bold)
-                console.log(' ')
-                console.log(('扫描的项目路径：' + self.stdouts.inputPath).cyan.bold)
-                console.log(('扫描报告主题：' + self.params.reportTitle).cyan.bold)
-                console.log(('结果报告目录路径：' + self.stdouts.outputPath).cyan.bold)
-                console.log(' ')
-                console.log(('查看扫描结果').gray)
-                console.log(('    查看项目中各文件汇总信息：' + self.stdouts.outputPath + '/display.html').gray)
-                console.log(('    查看项目文件具体信息：' + self.stdouts.outputPath + '/index.html').gray);
-            }
-
-            plato.inspect(allJsFiles, outputDir, options, callback)
+      // self.log(util.inspect(process.memoryUsage()))
             
+      self.log((self.params.reportTitle + 'JavaScript代码扫描完成......').green.bold)
+      self.log(' ')
+      self.log(('===================' + self.params.reportTitle + '基本信息===================').green.bold)
+      self.log(' ')
+      self.log(('扫描的项目路径：' + self.stdouts.inputPath).cyan.bold)
+      self.log(('扫描报告主题：' + self.params.reportTitle).cyan.bold)
+      self.log(('结果报告目录路径：' + self.stdouts.outputPath).cyan.bold)
+      self.log(' ')
+      self.log(('查看扫描结果').gray)
+      self.log(('    查看项目中各文件汇总信息：' + self.stdouts.outputPath + '/display.html').gray)
+      self.log(('    查看项目文件具体信息：' + self.stdouts.outputPath + '/index.html').gray)
+
+      // num++
+      // if(num < len) {
+      //     process.nextTick(function() {
+      //         plato.inspect(reports[num], outputDir + num, options, callback)
+      //     }, 10000)
+      // }
+    }
+
+    plato.inspect(allJsFiles, outputDir, options, callback)
+        //每25个文件一份报告
+    // if(jsFileNums > SPLIT_COUNT) {
+    //   for(var i = 0; i < len; i++) {
+    //     reports.push(allJsFiles.splice(0, SPLIT_COUNT))
+    //   }
+            
+    //   plato.inspect(reports[num], outputDir + num, options, callback)
+    // } else {
+    //   plato.inspect(allJsFiles, outputDir, options, callback)
+    // }
+  },
+  _getAllJsFiles: function(filePath, reg) {
+    var self = this
+    var result = []
+    var files = fs.readdirSync(filePath)
+
+    files.forEach(function(file){
+      var pathname = filePath + '/' + file
+      var stat = fs.lstatSync(pathname)
+      
+      //过滤要排除的文件或文件夹
+      if(!reg.test(file)) {
+        if (stat.isDirectory()){
+          result = result.concat(self._getAllJsFiles(pathname, reg))
+        } else {
+          var extname = path.extname(file)
+          if(extname === '.js') {
+            result.push(pathname)
+          }
         }
-        
-    },
-    // end: function () {
-    //     this.log(('===================' + this.params.reportTitle + '基本信息===================').cyan.bold)
-    //     this.log(('扫描的项目路径："' + this.stdouts.inputPath).green.bold)
-    //     this.log(('扫描报告主题：' + this.params.reportTitle).green.bold)
-    //     this.log(('结果报告目录路径：' + this.stdouts.outputPath).green.bold)
-    //     this.log(('查看扫描结果').gray)
-    //     this.log(('    查看项目中各文件汇总信息：' + this.stdouts.outputPath + '/display.html').gray)
-    //     this.log(('    查看项目文件具体信息：' + this.stdouts.outputPath + '/index.html').gray);
-    // },
-    _getAllJsFiles: function(filePath) {
-        var result = [];
-        var files = fs.readdirSync(filePath)
-        files.forEach(function(file){
-            var pathname = filePath + '/' + file
-            var stat = fs.lstatSync(pathname)
-
-            if (stat.isDirectory()){
-                var arr = ['node_modules', 'test']
-                if(arr.indexOf(file) == -1) {
-                    result = result.concat(getAllFiles(pathname));
-                }
-
-            } else {
-                var extname = path.extname(file)
-                if(extname === '.js') {
-                    result.push(pathname);
-                }
-            }
-        })
-        return result
-    },
+      }
+    })
+    return result
+  }
 })
--- generators/app/prompts.js
+++ generators/app/prompts.js
@@ -1,42 +1,108 @@
-var Inquirer = require('inquirer')
 
 module.exports = [
-    {
-        type: 'input',
-        message: '请输入扫描结果报告主题: ',
-        name: 'reportTitle',
-        default: 'XXX项目JS代码扫描结果报告',
-    },
-    {
-        type: 'input',
-        name: 'inputPath',
-        message: '请输入要扫描的项目路径(默认为当前目录): ',
-        default: process.cwd(),
-        //记住用户喜好
-        store: true
-    }, 
-    {
-        type: 'input',
-        message: '请输入扫描结果输出目录路径(默认为当前目录下output): ',
-        name: 'outputPath'
-    },
-    {
-        type: 'checkbox',
-        message: '是否过滤一下目录',
-        name: 'filterPath',
-        choices: [
-            new Inquirer.Separator(' ----- 可过滤目录 -----'),
-            {
-                name: 'node_modules'
-            },
-            {
-                name: 'test'
-            }
-        ]
-    },
-    {
-        type: 'confirm',
-        name: 'action',
-        message: '确定输入无误?'
-    }
-];
+  {
+    type: 'input',
+    message: '请输入扫描结果报告主题: ',
+    name: 'reportTitle',
+    default: 'XXX项目JS代码扫描结果报告',
+  },
+  {
+    type: 'confirm',
+    name: 'es6more',
+    message: '是否使用ES6/7语法?',
+    store: true,
+    default: true
+  },
+  {
+    type: 'confirm',
+    name: 'isReact',
+    message: '是否是React项目?',
+    store: true
+  },
+  {
+    type: 'confirm',
+    name: 'isJsx',
+    message: '是否使用JSX?',
+    store: true
+  },
+  {
+    type: 'input',
+    name: 'inputPath',
+    message: '请输入要扫描的项目目录(默认为当前目录): ',
+    default: process.cwd()
+  }, 
+  {
+    type: 'input',
+    message: '请输入扫描结果输出目录路径(默认为当前目录下reports): ',
+    name: 'outputPath',
+    default: process.cwd()
+  },
+  {
+    type: 'checkbox',
+    message: '是否可过滤以下目录, 请选择',
+    name: 'filterPath',
+    choices: [
+      {
+        value: '^node_modules$',
+        name: 'node_modules',
+        checked: true
+      },
+      {
+        value: '^build$',
+        name: 'build',
+        checked: true
+      },
+      {
+        value: '^dist$',
+        name: 'dist',
+        checked: true
+      },
+      {
+        value: '^public$',
+        name: 'public',
+        checked: true
+      },
+      {
+        value: '^logs?$',
+        name: 'log(s)',
+        checked: true
+      },
+      {
+        value: '^reports$',
+        name: 'reports',
+        checked: true
+      },
+      {
+        value: '^tests?$',
+        name: 'test(s)'
+      },
+      {
+        value: '^docs?$',
+        name: 'doc(s)'
+      },
+      {
+        value: '^examples?$',
+        name: 'example(s)'
+      },
+      {
+        value: '^install$',
+        name: 'install'
+      },
+      {
+        value: '^data$',
+        name: 'data'
+      }
+    ]
+  },
+  {
+    type: 'confirm',
+    name: 'file',
+    message: '是否过滤以.开头的文件或文件夹?',
+    default: true
+  },
+  {
+    type: 'confirm',
+    name: 'action',
+    message: '确定输入无误?'
+  }
+]
